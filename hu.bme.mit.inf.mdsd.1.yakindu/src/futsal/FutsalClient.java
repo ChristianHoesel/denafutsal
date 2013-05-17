84a0fa0036247f84788e860a7e1fc2ed161d99dc
Nádudvari György <reedcourty@gmail.com>
2013-05-17 21:10:46 +0000

    Kicsit módosítottam az SM-t, hogy a feltétel nélküli átmenetet is lehessen bizgetni. Meg egyéb dolgok, amikre már nem emlékszem.
    git-svn-id: https://modeling.inf.mit.bme.hu/svn/modeling_1_2013@94 89c263d7-b270-4681-9ff3-08a064455544

--- hu.bme.mit.inf.mdsd.1.yakindu/src/futsal/FutsalClient.java
+++ hu.bme.mit.inf.mdsd.1.yakindu/src/futsal/FutsalClient.java
@@ -6,16 +6,55 @@ import org.yakindu.scr.futsal_report_generator.Futsal_report_generatorStatemachi
 
 public class FutsalClient {
 
+	private static void Init() {
+		System.out.println(String
+				.format("Állapot: %s", State.match_region_Init));
+		// TODO Ide jönne a Match inicializálása
+	}
+
+	private static void sleep(Futsal_report_generatorStatemachine sm, int limit)
+			throws InterruptedException {
+		for (int i = 0; i < limit; i++) {
+			Thread.sleep(100);
+			sm.runCycle();
+		}
+	}
+
 	public static void main(String[] args) throws InterruptedException {
+		// Inicializálás:
 		Futsal_report_generatorStatemachine sm = new Futsal_report_generatorStatemachine();
 		sm.setTimerService(new TimerService());
 		sm.enter();
+
 		if (sm.isStateActive(State.match_region_Init)) {
-			System.out.println("Itt vagyunk az Init-ben");
+			Init();
 		}
 		
 
-		System.out.println(sm.isStateActive(State.match_region_Waiting_For_the_Start_of_1st_Halftime));
+		sm.getSCIController().raiseNext_state();
+		sm.runCycle();
 
+		if (sm.isStateActive(State.match_region_Waiting_For_the_Start_of_1st_Halftime)) {
+			System.out.println(String.format("Állapot: %s",
+					State.match_region_Waiting_For_the_Start_of_1st_Halftime));
+		}
+		sm.runCycle();
+
+		sm.getSCITimer().raiseContinue();
+		sm.runCycle();
+
+		if (sm.isStateActive(State.match_region__1st_Halftime)) {
+			System.out.println(String.format("Állapot: %s",
+					State.match_region__1st_Halftime));
+			sm.runCycle();
+
+			sleep(sm, 10);
+			System.out.println(sm.getSCITimer().getTick());
+		}
+		sm.runCycle();
+
+		sm.exit();
+
+		System.exit(0);
 	}
 }
