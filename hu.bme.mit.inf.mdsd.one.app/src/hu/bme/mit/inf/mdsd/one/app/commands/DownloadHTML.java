f25b93e28f16c03180ed2656a854c15bb7d26473
Nádudvari György <reedcourty@gmail.com>
2013-05-27 19:37:32 +0000

    Generator menü és metódus vázak.
    git-svn-id: https://modeling.inf.mit.bme.hu/svn/modeling_1_2013@124 89c263d7-b270-4681-9ff3-08a064455544

---
+++ hu.bme.mit.inf.mdsd.one.app/src/hu/bme/mit/inf/mdsd/one/app/commands/DownloadHTML.java
@@ -0,0 +1,19 @@
+package hu.bme.mit.inf.mdsd.one.app.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DownloadHTML extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		//IPreferenceStore preferenceStore = PlatformUI.getPreferenceStore();
+		//System.out.println("FRDF " + preferenceStore.getString(PreferencesPage.OUT_FOLD));
+		//generator.generateDataModelToOutputFolder(model, preferenceStore.getString(PreferencesPage.OUT_FOLD));
+				
+		return null;
+	}
+
+}
---
+++ hu.bme.mit.inf.mdsd.one.app/src/hu/bme/mit/inf/mdsd/one/app/commands/GenerateTeXReport.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.inf.mdsd.one.app.commands;
+
+public class GenerateTeXReport {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
